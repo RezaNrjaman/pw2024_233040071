e8c905582a6cd9d49959a94e0532c63a69f373f8
RezaNrjaman <rezanrjaman121204@gmail.com>
2024-02-19 23:32:54 +0700

    tugas1 selesai

---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,13 @@
+<?php
+$npm = "71";
+
+echo "Aku adalah angka <b>$npm</b>";
+echo "<br/>";
+echo "Jika aku dikali 5, maka aku sekarang menjadi" . ' ' . '<b>' . $npm * 5 . '</b>';
+echo "<br/>";
+echo "Jika aku dibagi 2, maka aku sekarang menjadi" . ' ' . '<b>' . $npm * 5 / 2 . '</b>';
+echo "<br/>";
+echo "Jika aku ditambah 75, maka aku sekarang menjadi" . ' ' . '<b>' . $npm * 5 / 2 + 75 . '</b>';
+echo "<br/>";
+echo "Jika aku dikurang 20, maka aku sekarang menjadi" . ' ' . '<b>' . $npm * 5 / 2 + 75 - 20 . '</b>';
+?>
