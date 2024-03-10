b3b9d25076d17b4df89eb88165acb0a54c11da8c
RezaNrjaman <rezanrjaman121204@gmail.com>
2024-03-10 13:32:53 +0700

    upload tugas

--- tugas/tugas4/4b.php
+++ tugas/tugas4/4b.php
@@ -1,31 +1,33 @@
-<?php 
+<?php
 $komponen = ["Motherboard", "Processor", "Hard Disk", "PC Coller", "VGA Card", "SSD"];
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tugas - 4b</title>
+    <title>Tugas</title>
 </head>
+
 <body>
     <h2>Macam-macam perangkat keras Komputer</h2>
     <ol>
-        <?php foreach($komponen as $komp):?>
-        <li><?= $komp?></li>
+        <?php foreach ($komponen as $k) : ?>
+            <li><?= $k ?></li>
         <?php endforeach; ?>
     </ol><br>
 
-    <!-- yang di tambah dan di urutkan  -->
     <h2>Macam-macam perangkat keras Komputer baru</h2>
     <ol>
-        <?php array_push($komponen, "Card Reader", "Modem"); asort($komponen) ?>
-        <?php foreach($komponen as $komp):?>
-        <li><?= $komp?></li>
+        <?php array_push($komponen, "Card Reader", "Modem");
+        asort($komponen) ?>
+        <?php foreach ($komponen as $k) : ?>
+            <li><?= $k ?></li>
         <?php endforeach; ?>
     </ol>
 
-
 </body>
+
 </html>
